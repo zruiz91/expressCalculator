82d71296673a1e055e6039bb454a4de290fed805
Zach Ruiz <powersbooth10@gmail.com>
2023-04-20 12:13:39 -0500

    added some logice to add together the two numbers on the server side and send back a sum.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 
 const express = require('express');
 
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
     console.log("listening on port 3000");
@@ -9,4 +13,13 @@ app.listen(3000, function() {
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
-})
+});
+
+app.post('/', function(req, res){
+    let num1 = Number(req.body.n1);
+    let num2 = Number(req.body.n2);
+    let result = num1 + num2;
+
+    res.send("The answer is: " + result);
+    console.log(req.body.num1);
+});
