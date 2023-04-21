d7471341829b6a7fb34ebcb14d33bfcf6df1808c
Zach Ruiz <powersbooth10@gmail.com>
2023-04-21 12:44:54 -0500

    added the get and post routes for the bmiCalculator page.

--- calculator.js
+++ calculator.js
@@ -15,11 +15,26 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+
+// route that gets the bmiCalculator page
+app.get('/bmiCalculator', function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
 app.post('/', function(req, res){
     let num1 = Number(req.body.n1);
     let num2 = Number(req.body.n2);
     let result = num1 + num2;
 
     res.send("The answer is: " + result);
-    console.log(req.body.num1);
+});
+
+
+//route that posts the data entered into the inputs and sends back the BMI of the user
+app.post('/bmiCalculator', function(req, res){
+    let weight = Number(req.body.weight);
+    let height = Number(req.body.height);
+    let BMI = (weight) / (height * height)
+
+    res.send("Your BMI is: " + BMI);
 });
